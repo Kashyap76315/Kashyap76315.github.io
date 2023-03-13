227701d1eaa9f14aa18da40551a3012efab48bb8
Kashyap76315 <kpate209@uncc.edu>
2023-03-13 18:19:08 -0400

    8th Push

--- private_html/itis3135/scripts/header.js
+++ private_html/itis3135/scripts/header.js
@@ -9,11 +9,17 @@ document.addEventListener("DOMContentLoaded", function() {
         <a href="introduction.html">Introduction</a> |
         <a href="tables.html">HTML Tables</a> |
         <a href="forms.html">HTML Forms</a> |
-        <a href="stuff/KRAPPYCRAPPYWE.htm">Crappy Website</a> |
         <a href="website_evaluations.html">Website Evaluation</a> |
         <a href="first-scripts.html">First Script</a> |
-        <a href="calculator_try.html">Calculator</a> |
+        <a href="calculator_try.html">Calculator Try</a> |
+        <a href="calculator_fcc.html">Calculator FCC</a> |
         <a href="polygons.html">Polygons</a> |
+        <a href="arrays.html">Arrays</a> |
+        <a href="contract.html">Contract</a>
+      </nav>
+      <nav style="padding: 2%;">
+      <a href="stuff/ KRAPPYCRAPPYWE.htm">Crappy Website</a> |
+      <a href="">Accessibility</a>
       </nav>
     `;
     document.body.insertBefore(header, document.body.firstChild);
